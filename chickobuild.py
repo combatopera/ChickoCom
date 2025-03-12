fb4eadac7be863bb8d0b5669d8273339bf4afb53
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2025-03-12 14:02:39 +0000

    compile statline picture

--- chickobuild.py
+++ chickobuild.py
@@ -1,15 +1,25 @@
 from PIL import Image
 from struct import pack
 
+chunksize = 16
+
 def _encodecomponent(ste):
     return ((ste & 0x1) << 3) | ((ste >> 1) & 0x7)
 
 def main():
-    with Image.open('statline.png') as f, open('statline.pi1', 'wb') as g:
+    with Image.open('statline.png') as im, open('statline.pi1', 'wb') as g:
         g.write(b'\0\0')
-        palette = [_encodecomponent(round(component / 0xff * 0xf)) for component in f.getpalette()]
+        palette = [_encodecomponent(round(component / 0xff * 0xf)) for component in im.getpalette()]
         for i in range(16):
             g.write(pack('=BB', palette[i * 3], (palette[i * 3 + 1] << 4) | palette[i * 3 + 2]))
+        data = im.getdata()
+        for k in range(0, 320 * 200, chunksize):
+            words = [0] * 4
+            for i in range(k, k + chunksize):
+                value = data[i]
+                for b in range(4):
+                    words[b] = (words[b] << 1) | ((value >> b) & 0x1)
+            g.write(pack('>HHHH', *words))
 
 if '__main__' == __name__:
     main()
