971870b6029a654480eea17186b2677cbf218286
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2025-03-12 12:50:57 +0000

    quick and dirty build script wip

---
+++ chickobuild.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from struct import pack
+
+def _encodecomponent(ste):
+    return ((ste & 0x1) << 3) | ((ste >> 1) & 0x7)
+
+def main():
+    with Image.open('statline.png') as f, open('statline.pi1', 'wb') as g:
+        g.write(b'\0\0')
+        palette = [_encodecomponent(round(component / 0xff * 0xf)) for component in f.getpalette()]
+        for i in range(16):
+            g.write(pack('=BB', palette[i * 3], (palette[i * 3 + 1] << 4) | palette[i * 3 + 2]))
+
+if '__main__' == __name__:
+    main()
